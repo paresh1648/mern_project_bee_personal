d9978bd4b5eb7e43d67756bb8fc0f37202f9e10e
Naman754 <naman0880.be20@chitkara.edu.in>
2022-11-17 10:34:39 +0530

    MERN CODE

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+    const app = express(); 
+    const hbs = require("hbs"); 
+    require("./db/conn");
+    const port = process.env.PORT || 3000;
+    const static_path = path.join(__dirname, "../public");
+    const template_path = path.join(__dirname, "../templates"); 
+    const partials_path = path.join(__dirname, "../partials"); 
+    app.use(express.static(static_path));
+    app.set("view engine", "hbs");
+    app.set("views", template_path); 
+    hbs.registerPartials(partials_path); 
+    console.log(path.join(__dirname, "../public")); 
+    // const template_path = path.join(__dirname, "../templates/views"); 
+        app.get("/", (req, res) => {
+            res.render("index");
+        });
+        app.get("/register", (req, res) => {
+            res.render("register");
+            });
+        
+            app.get("/login",(req,res)=>{
+               res.render("login");
+            })
+
+        app.listen(port, () => {
+            console.log(`server is running at port no ${port}`);
+        })
